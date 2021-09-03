c5db6c128ed0671e917560524f9ac83d15cf4655
Juandiacosta <acostasaenzjuandiego@gmail.com>
2021-09-03 00:38:33 -0500

    Matrícula Unad
    Quitando el pass

--- calculo_matricula.py
+++ calculo_matricula.py
@@ -9,13 +9,13 @@ def run():
             self.estrato_estudiante = ""
             self.nombre_estudiante = ""
             self.numero_creditos = 0
-            pass
+            
 
         def Datos_estudiante(self):
             self.nombre_estudiante = input('Ingrese su nombre: ')
             self.estrato_estudiante = input('Ingrese el estrato al que pertenece: ')
             self.numero_creditos = int(input(('Ingrese la cantidad de créditos que desea matricular: ')))
-            pass
+            
 
         def Calculo_matricula(self):
             self.estrato_estudiante = int(self.estrato_estudiante)
@@ -27,16 +27,14 @@ def run():
                 pass
             else:
                 print('El estrato ingresado no es válido')
-                pass
-            pass
+                
 
         def Desprendible_matricula(self):
             self.nombre_estudiante = self.nombre_estudiante.title()
             self.estrato_estudiante = str(self.estrato_estudiante)
             self.valor_matricula = str(self.valor_matricula)
             print('El estudiante ' + self.nombre_estudiante + ' perteneciente al estrato ' + self.estrato_estudiante + ' pagará por matrícula $' + self.valor_matricula)
-            pass
-        pass
+            
 
     Matricula = CalculoMatricula()
     Matricula.Datos_estudiante()
@@ -46,6 +44,6 @@ def run():
 
 if __name__ == "__main__":
     run()
-    pass
+    
 
 msvcrt.getch()
